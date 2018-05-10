f4aa06b25a0e280346627adadd3e16e7925ba5cf
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-05-10 10:16:47 -0300

    Initial commit.

---
+++ src/main/java/quem/me/ajuda/QuemMeAjudaApplication.java
@@ -0,0 +1,12 @@
+package quem.me.ajuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemMeAjudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemMeAjudaApplication.class, args);
+	}
+}
---
+++ src/test/java/quem/me/ajuda/QuemMeAjudaApplicationTests.java
@@ -0,0 +1,16 @@
+package quem.me.ajuda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuemMeAjudaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
