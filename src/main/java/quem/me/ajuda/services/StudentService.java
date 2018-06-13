38ab6c947218863845e13f7de946288cc49dbb7d
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-06-13 12:51:41 -0300

    Encode user passwords on database.

--- src/main/java/quem/me/ajuda/services/StudentService.java
+++ src/main/java/quem/me/ajuda/services/StudentService.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +17,10 @@ public class StudentService {
 	@Autowired
 	private StudentRepository repository;
 	
+	private final Integer gemSalt = 4;
+	
 	public Student create(Student student) {
+		student.setPassword(BCrypt.hashpw(student.toString(), BCrypt.gensalt(gemSalt)));
 		return this.repository.save(student);
 	}
 	
