3482a237563d7c3e42a4e1e05650df19fdb7645a
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-06-30 00:04:48 +0000

    Grant that passed id and token refer to same user.

---
+++ src/main/java/quem/me/ajuda/constants/CacheKeys.java
@@ -0,0 +1,5 @@
+package quem.me.ajuda.constants;
+
+public class CacheKeys {
+	public static final String STUDENTS = "students";
+}
--- src/main/java/quem/me/ajuda/exceptions/UserNotFoundException.java
+++ src/main/java/quem/me/ajuda/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package quem.me.ajuda.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends EntityNotFoundException {
 	private static final long serialVersionUID = 1L;
 		
