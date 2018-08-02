39362fd5bb7984290a8f1a88348d459e9ed748be
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-02 11:23:06 -0300

    Add base test for login request.

--- src/test/java/quem/me/ajuda/controllers/AuthenticationControllerTest.java
+++ src/test/java/quem/me/ajuda/controllers/AuthenticationControllerTest.java
@@ -3,8 +3,10 @@
  */
 package quem.me.ajuda.controllers;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -14,6 +16,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -25,7 +29,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import quem.me.ajuda.constants.Endpoints;
 import quem.me.ajuda.models.Student;
 import quem.me.ajuda.security.model.UserCredentials;
-import quem.me.ajuda.services.StudentService;
+import quem.me.ajuda.services.AuthenticationService;
 
 /**
  *
@@ -37,30 +41,30 @@ class AuthenticationControllerTest {
 	private final String ENDPOINT = "/".concat(Endpoints.LOGIN_ENDPOINT);
 
 	private Student testUser;
-	
 	private UserCredentials testCredentials;
-	
 	private MockMvc mockMvc;
 	
 	@Autowired
-	private StudentService studentService;
-	
-	@Autowired
 	private ObjectMapper objectMapper;
+	
+	@Mock
+	private AuthenticationService authService;
+	
+	@InjectMocks
+	private AuthenticationController authController;
 
 	/**
 	 * 
 	 */
 	@BeforeAll
 	void setUpAll() {
-		this.mockMvc = standaloneSetup(AuthenticationController.class).build();
+		this.mockMvc = standaloneSetup(authController).build();
 	}
 	
 	@BeforeEach
 	void setUp() {
 		this.testUser = new Student("user", "pass", "phone", "email", "password");
 		this.testCredentials = new UserCredentials(testUser.getRegistration(), testUser.getPassword());
-		this.studentService.create(testUser);
 	}
 
 	/**
@@ -68,11 +72,15 @@ class AuthenticationControllerTest {
 	 */
 	@Test
 	void testLoginSucessfully() throws Exception {
+		when(authService.authenticate(testCredentials))
+			.thenReturn(eq(testUser));
+		when(authService.tokenFor(testUser))
+			.thenReturn(anyString());
+		
 		mockMvc.perform(post(ENDPOINT)
 				.contentType(MediaType.APPLICATION_JSON)
 				.characterEncoding("UTF-8")
 				.content(objectMapper.writeValueAsString(testCredentials)))
-				.andDo(print())
 				.andExpect(status().isOk());
 	}
 
